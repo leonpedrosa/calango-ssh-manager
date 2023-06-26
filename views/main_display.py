6fb2c2bbb2d7a66e48c63ee041c9f5c8b0e3e34b
leonpedrosa <leon.oliveira.pedrosa@gmail.com>
2023-06-25 21:45:43 -0300

    - Adicionando válidações para os campos - Refatorando código

--- views/main_display.py
+++ views/main_display.py
@@ -20,11 +20,11 @@ class DialogConfirm(QDialog):
         label.setFont(font)
         label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
 
-        yes_button = QPushButton(text='Sim', icon=QIcon(f'{path_icon_button}accept.ico'))
+        yes_button = QPushButton(text='Sim', icon=QIcon(f'{path_icons}accept.ico'))
         yes_button.setFixedSize(120, 50)
         yes_button.clicked.connect(self.accept)
 
-        no_button = QPushButton(text='Não', icon=QIcon(f'{path_icon_button}not_accept.png'))
+        no_button = QPushButton(text='Não', icon=QIcon(f'{path_icons}not_accept.png'))
         no_button.setFixedSize(120, 50)
         no_button.clicked.connect(self.reject)
 
@@ -43,22 +43,24 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(QIcon(path_icon_windows))
         self.setMinimumSize(1020, 720)
         # self.showMaximized()
-        self.barraMenu()
+        self.barra_menu()
 
         self.show()
 
-    def barraMenu(self):
+    def barra_menu(self):
         menu_bar = self.menuBar()
         file = menu_bar.addMenu('&Arquivo')
-        # btn_new_connect_ssh = Btn(file)
-        # file.addAction(btn_new_connect_ssh)
-        bt_new_conn = QAction(
-            QIcon(f'{path_icons}connect-ssh.png'),
-            'Nova Conexão',
-            self
-        )
-        bt_new_conn.triggered.connect(self.newConnect)
+
+        bt_new_conn = QAction(QIcon(f'{path_icons}connect.png'), 'Nova Conexão', self )
+        bt_new_conn.triggered.connect(self.new_connect_ssh)
         file.addAction(bt_new_conn)
+
+        bt_conn = QAction('Conexão Direta', self)
+        bt_conn.triggered.connect(self.direct_connect)
+        file.addAction(bt_conn)
+
+        # bt_del_conn = QAction(QIcon(f'{path_icons}desconect.png'), 'Deletar Conexão', self )
+        # file.addAction(bt_del_conn)
 
         file.addSeparator()
 
@@ -66,19 +68,25 @@ class MainWindow(QMainWindow):
         file.addAction(file_sub_menu)
         file_sub_menu.triggered.connect(self.sair)
 
-    def newConnect(self):
+    def new_connect_ssh(self):
         new_connect = QDialog(self)
         new_connect.setWindowTitle('Nova Conexão')
         new_connect.setWindowIcon(QIcon(path_icon_windows))
         # new_connect.setMinimumSize(600, 720)
-        new_connect.setFixedSize(300, 200)
+        new_connect.setFixedSize(400, 280)
 
         line_nome, line_host, line_port, line_usuario, line_senha = (QLineEdit() for _ in range(5))
 
         int_validator = QIntValidator()
         line_port.setValidator(int_validator)
 
-        save_btn = QPushButton(text='Salvar', icon=QIcon(f'{path_icon_button}save.png'))
+        line_senha.setEchoMode(QLineEdit.EchoMode.Password)
+        line_senha.setMaximumWidth(120)
+
+        line_port.setMaximumWidth(120)
+
+        save_btn = QPushButton(text='Salvar', icon=QIcon(f'{path_icons}save.png'))
+        save_btn.setMaximumWidth(80)
         save_btn.clicked.connect(lambda: self.save(
                 {
                     "nome": line_nome.text(),
@@ -86,31 +94,49 @@ class MainWindow(QMainWindow):
                     "port": line_port.text(),
                     "usuario": line_usuario.text(),
                     "senha": line_senha.text()
-                }
+                },
+                new_connect
             )
         )
-        save_btn.clicked.connect(new_connect.close)
+        # save_btn.clicked.connect(new_connect.close)
 
-        cancel_btn = QPushButton(text='Cancel', icon=QIcon(f'{path_icon_button}not_accept.png'))
+        cancel_btn = QPushButton(text='Cancel', icon=QIcon(f'{path_icons}not_accept.png'))
+        cancel_btn.setMaximumWidth(80)
         cancel_btn.clicked.connect(new_connect.close)
+
+        v_pass_btn = QPushButton(icon=QIcon(f'{path_icons}show-password.png'))
+        v_pass_btn.setMaximumWidth(30)
+        v_pass_btn.setCheckable(True)
+
+        def hide_show_password():
+            if v_pass_btn.isChecked():
+                line_senha.setEchoMode(QLineEdit.EchoMode.Password)
+                v_pass_btn.setIcon(QIcon(f'{path_icons}hide-password.png'))
+            else:
+                line_senha.setEchoMode(QLineEdit.EchoMode.Normal)
+                v_pass_btn.setIcon(QIcon(f'{path_icons}show-password.png'))
+
+        v_pass_btn.clicked.connect(hide_show_password)
 
         layout = QGridLayout()
         new_connect.setLayout(layout)
-        layout.addWidget(QLabel('Nome: '), 0, 0, 1, 2)
-        layout.addWidget(line_nome, 1, 0, 1, 2)
+        layout.addWidget(QLabel('Nome: '), 0, 0, 1, 3)
+        layout.addWidget(line_nome, 1, 0, 1, 4)
 
         layout.addWidget(QLabel('Host: '), 2, 0)
-        layout.addWidget(QLabel('Port: '), 2, 1)
-        layout.addWidget(line_port, 3, 1)
-        layout.addWidget(line_host, 3, 0)
+        layout.addWidget(QLabel('Port: '), 2, 3)
+        layout.addWidget(line_port, 3, 3)
+        layout.addWidget(line_host, 3, 0, 1, 3)
         layout.addWidget(QLabel('Usuário: '), 4, 0)
-        layout.addWidget(line_usuario, 5, 0)
-        layout.addWidget(QLabel('Senha: '), 4, 1)
-        layout.addWidget(line_senha, 5, 1)
+        layout.addWidget(line_usuario, 5, 0, 1, 3)
+        layout.addWidget(QLabel('Senha: '), 4, 3)
+        layout.addWidget(line_senha, 5, 3)
 
         layout.addWidget(QLabel(), 6, 0)
-        layout.addWidget(save_btn, 7, 0)
-        layout.addWidget(cancel_btn, 7, 1)
+        layout.addWidget(v_pass_btn, 6, 3)
+        layout.addWidget(QLabel(), 7, 0)
+        layout.addWidget(save_btn, 8, 1, alignment=Qt.AlignmentFlag.AlignRight)
+        layout.addWidget(cancel_btn, 8, 3)
 
         new_connect.setTabOrder(line_nome, line_host)
         new_connect.setTabOrder(line_host, line_port)
@@ -121,23 +147,31 @@ class MainWindow(QMainWindow):
 
         new_connect.exec()
 
-    def save(self, *args, **kwargs):
+    def direct_connect(self):
+        print('Direto')
+
+    def validate_fields(self, fields):
+        for field_name, field_value in fields.items():
+            if not field_value:
+                QMessageBox.warning(self, 'Campo Vazio', f'O campo "{field_name}" não pode ficar vazio.')
+                return False
+        return True
+
+    def save(self, fields, new_connect, *args, **kwargs):
         check_sqlite_exist()
-        conn = sqlite3.connect(f'{path_sqlite}')
-        cursor = conn.cursor()
+        if self.validate_fields(fields):
+            conn = sqlite3.connect(f'{path_sqlite}')
+            cursor = conn.cursor()
 
-        query = f'''
-        INSERT INTO conexoes (nome, host, port, usuario, senha)
-        VALUES ('{args[0]['nome']}', '{args[0]['host']}', '{args[0]['port']}', '{args[0]['usuario']}', '{args[0]['senha']}')
-        '''
+            query = f'''INSERT INTO conexoes (nome, host, port, usuario, senha) 
+            VALUES ('{fields['nome']}', '{fields['host']}', '{fields['port']}', '{fields['usuario']}', '{fields['senha']}')'''
+            cursor.execute(query)
 
-        cursor.execute(query)
-
-        conn.commit()
-        conn.close()
-
-
-
+            conn.commit()
+            conn.close()
+            new_connect.close()
+        else:
+            return False
 
     def sair(self):
         confirm = DialogConfirm()
@@ -149,6 +183,4 @@ class MainWindow(QMainWindow):
 
 qt = QApplication(sys.argv)
 app = MainWindow()
-# app.show()
-# qt.exec()
 sys.exit(qt.exec())
--- views/support/paths.py
+++ views/support/paths.py
@@ -1,4 +1,3 @@
 path_icon_windows = 'views/assets/icon_window/calango32x32.png'
-path_icon_button = 'views/assets/icon_dialog/'
 path_icons = 'views/assets/icon_menu/'
 path_sqlite = 'views/sqlite.db'
